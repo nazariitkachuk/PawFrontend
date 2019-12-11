e29b6437c5646fadff5dfb17f0673165951715a4
209278 <209278@edu.p.lodz.pl>
2019-12-11 12:52:22 +0100

    add card fix

--- src/Main/List/AddNode/AddNode.js
+++ src/Main/List/AddNode/AddNode.js
@@ -9,9 +9,9 @@ export default class AddNode extends React.Component{
     
     render(){
         return React.createElement("div", {class: "addNewNodeWrapper"},
-                    React.createElement("input", {type: "text", id: "newNodeTitleInput"}),
+                    React.createElement("input", {type: "text", id: "newNodeTitleInput" + this.listId}),
                     React.createElement("input", {type: "submit", value: "Add", id: "newNodeAddButton", 
                         onClick: () => RESTrequests.addNewCard(this.tableId, 
-                            this.listId, document.getElementById("newNodeTitleInput").value) }));
+                            this.listId, document.getElementById("newNodeTitleInput" + this.listId).value) }));
     }
 }
